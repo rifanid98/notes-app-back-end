4717775886731bf961cdb7441769c806c0f7b35b
rifanid <adninsijawa.office@gmail.com>
2021-05-15 19:51:45 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+/* eslint-disable import/extensions */
+import Hapi from '@hapi/hapi';
+import routes from './routes.js';
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  // eslint-disable-next-line no-console
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
