e47fe4b43900d4e22960a5ce60575b43c5d931f9
rifanid <adninsijawa.office@gmail.com>
2021-05-15 20:34:00 +0700

    update ip

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import routes from './routes.js';
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.28.123',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.27.7',
     routes: {
       cors: {
         origin: ['*'],
